007048ffb28e4350aaeb94e5e9ae73a3a99b60da
DEV-COM <kwp@tlen.pl>
2023-11-29 12:13:05 +0100

    dispatch...

---
+++ 4/worker.cpp
@@ -0,0 +1,26 @@
+#include "worker.h"
+#include "dispatcher.h"
+#include <chrono>
+
+using namespace std;
+
+void Worker::getCondition(condition_variable* &cv){
+    cv = &(this)->cv;
+}
+
+void Worker::run(){
+    while (running) {
+        if (ready){
+            ready = false;
+            request->process();
+            request->finish();
+        }
+        if (Dispatcher::addWorker(this)){
+            while (!ready && running){
+                if (cv.wait_for(ulock, chrono::seconds(1)) == cv_status::timeout){
+
+                }
+            }
+        }
+    }
+}
