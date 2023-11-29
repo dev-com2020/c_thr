603c16e21d059a2efca0529986bbe11e914fe155
DEV-COM <kwp@tlen.pl>
2023-11-29 11:58:11 +0100

    request.h

---
+++ 4/request.cpp
@@ -0,0 +1,9 @@
+#include "request.h"
+
+void Request::process(){
+    outFnc("Starting processing request " + std::to_string(value) + "...");
+}
+
+void Request::finish(){
+    outFnc("Finished request " + std::to_string(value));
+}
